3f1605268ea89f6e5c6f093b04a3fee70fc6e4d6
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-08 13:20:06 +0900

    Add viewPager installation in MainActivity

--- app/src/main/java/uz/behzoddev/viewpager2sample/MainActivity.kt
+++ app/src/main/java/uz/behzoddev/viewpager2sample/MainActivity.kt
@@ -2,10 +2,40 @@ package uz.behzoddev.viewpager2sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import uz.behzoddev.viewpager2sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // Create the object of ViewPager2
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewPager2Adapter: ViewPager2Adapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViewPager2()
+        registerOnPageChange()
+    }
+
+    private fun setupViewPager2() = with(binding) {
+        viewPager2 = binding.viewPager
+        viewPager2Adapter = ViewPager2Adapter(this@MainActivity)
+        viewPager2.adapter = viewPager2Adapter
+    }
+
+    private fun registerOnPageChange() {
+        viewPager2.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
+        {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+            }
+        })
     }
 }
